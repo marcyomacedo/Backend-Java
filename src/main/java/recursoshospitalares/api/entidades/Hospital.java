6cb828d7c9310ec69f8ec4ad2cd385f9a5b344a4
Márcio Filho <51715526+marcyomacedo@users.noreply.github.com>
2021-09-13 16:25:25 -0300

    Update 1
    Ajustes iniciais(H2 e entidades)

---
+++ src/main/java/recursoshospitalares/api/entidades/Hospital.java
@@ -0,0 +1,20 @@
+package recursoshospitalares.api.entidades;
+
+
+public class Hospital {
+	
+	private Long cnpj;
+	private String nome;
+	private String endereco;
+	private String localizacao;	
+	
+	private int medico;
+	private int enfermeiro;
+	private int respirador;
+	private int tomografo;
+	private int ambulancia;
+	private Double taxaOcupacao;
+	
+	
+
+}
